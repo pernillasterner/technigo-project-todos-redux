f6fbfd17f943087b99b028bbe1fa9dda9bbf3ec3
psterner <psterner@hotmail.com>
2023-11-22 10:52:52 +0100

    Create store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+const reducer = combineReducers({
+  task: task.reducer,
+  project: project.reducer,
+  filter: filter.reducer,
+});
+
+const store = configureStore({
+  reducer,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
