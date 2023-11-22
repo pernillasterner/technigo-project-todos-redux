d95587cd4827a47a1bd7365be234810b050dac18
psterner <psterner@hotmail.com>
2023-11-22 14:03:39 +0100

    Add TopLeftIcon and styling to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import styled from "styled-components";
+import { TopLeftIcon } from "./components/Header/TopLeftIcon";
 import { Header } from "./components/Header/Header";
+
+const MAIN = styled.main`
+  padding: var(--general-padding);
+`;
 
 export const App = () => {
   return (
-    <main>
+    <MAIN>
+      <TopLeftIcon />
       <Header />
-    </main>
+    </MAIN>
   );
 };
