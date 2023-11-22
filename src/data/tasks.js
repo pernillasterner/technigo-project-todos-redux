a3d0a8144295f42d5862677294a22a8fd2cdf0a1
psterner <psterner@hotmail.com>
2023-11-22 12:00:55 +0100

    Create task object

---
+++ src/data/tasks.js
@@ -0,0 +1,29 @@
+export const tasks = [
+  {
+    id: 1,
+    title: "Fix laundry 🧺",
+    created_at: "2023-01-01",
+    categories: ["housecleaning", "high"],
+    due_date: "",
+    completed: false,
+    color: "blue",
+  },
+  {
+    id: 2,
+    title: "Pay bills 💸",
+    created_at: "2023-02-02",
+    categories: [],
+    due_date: "2023-04-04",
+    completed: true,
+    color: "green",
+  },
+  {
+    id: 3,
+    title: "Learn Redux",
+    created_at: "2023-02-02",
+    categories: ["Studdies", "medium"],
+    due_date: "2023-04-04",
+    completed: false,
+    color: "blue",
+  },
+];
