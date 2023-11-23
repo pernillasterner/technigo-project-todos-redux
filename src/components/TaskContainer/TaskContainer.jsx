8938c6c2786b337fd95df8b3328d9bdd198c0dca
psterner <psterner@hotmail.com>
2023-11-23 18:54:55 +0100

    Add component  EditTask

--- src/components/TaskContainer/TaskContainer.jsx
+++ src/components/TaskContainer/TaskContainer.jsx
@@ -2,6 +2,8 @@ import { useSelector } from "react-redux";
 import { TaskColumn } from "./TaskColumn";
 import styled from "styled-components";
 import { NoTasks } from "./ClearTasks/NoTasks";
+import { EditTask } from "./EditTask/EditTask";
+import { openEditModal } from "../../reducers/modal/modalSlice";
 
 const StyledTaskContainer = styled.div.attrs((props) => ({
   className: props.className || "",
@@ -10,10 +12,12 @@ const StyledTaskContainer = styled.div.attrs((props) => ({
   justify-content: space-around;
   flex-wrap: wrap;
   height: 100vh;
+  position: relative;
 `;
 
 export const TaskContainer = () => {
   const tasks = useSelector((store) => store.task.tasks);
+
   const taskColumns = [
     { title: "uncompleted", tasks: tasks.filter((task) => !task.completed) },
     { title: "completed", tasks: tasks.filter((task) => task.completed) },
@@ -33,6 +37,7 @@ export const TaskContainer = () => {
       ) : (
         <NoTasks />
       )}
+      {!openEditModal && <EditTask />}
     </StyledTaskContainer>
   );
 };
