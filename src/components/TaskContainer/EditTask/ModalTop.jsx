ba3626e193b7f6615df896b8b13258e891c81e13
psterner <psterner@hotmail.com>
2023-11-24 09:38:14 +0100

    Add new cats. Add styling

--- src/components/TaskContainer/EditTask/ModalTop.jsx
+++ src/components/TaskContainer/EditTask/ModalTop.jsx
@@ -1,15 +1,30 @@
 import styled from "styled-components";
+import { CatBtn } from "../../../styles/Buttons";
 
-export const ModalTop = ({ onCategoryChange, categories }) => {
+const CatInputContainer = styled.div.attrs((props) => ({
+  className: props.className || "",
+}))`
+  width: 90%;
+`;
+
+const CatsContainer = styled.div.attrs((props) => ({
+  className: props.className || "",
+}))`
+  margin-bottom: 0.7rem;
+`;
+
+export const ModalTop = ({ onCategoryChange, cats }) => {
   const handleCatChange = (e) => {
     e.preventDefault();
     const newCat = e.target.value;
     onCategoryChange(newCat);
+
+    // TODO: Need to clear the input field
   };
 
   return (
     <>
-      <div className="cat_input-close">
+      <CatInputContainer className="cat_input-close">
         <label htmlFor="category" className="is-hidden">
           Category
         </label>
@@ -19,12 +34,10 @@ export const ModalTop = ({ onCategoryChange, categories }) => {
           placeholder="Add category"
           onChange={handleCatChange}
         />
-      </div>
-      <div className="cats">
-        {categories.map((cat, index) => (
-          <span key={index}>{cat}</span>
-        ))}
-      </div>
+      </CatInputContainer>
+      <CatsContainer className="cats">
+        {cats && cats.map((cat, index) => <CatBtn key={index}>{cat}</CatBtn>)}
+      </CatsContainer>
       {/* ... rest of the component */}
     </>
   );
