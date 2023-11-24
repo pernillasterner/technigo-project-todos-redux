9e49709e386c0f8ea8262d91056176b9d3e321ef
psterner <psterner@hotmail.com>
2023-11-24 09:22:50 +0100

    Make component of catbtn

--- src/components/TaskContainer/TaskCard/CardTop.jsx
+++ src/components/TaskContainer/TaskCard/CardTop.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { openEditModal } from "../../../reducers/modal/modalSlice";
+import { CatBtn } from "../../../styles/Buttons";
 
 const Top = styled.div.attrs((props) => ({
   className: props.className || "",
@@ -9,20 +10,6 @@ const Top = styled.div.attrs((props) => ({
   justify-content: space-between;
   align-items: center;
   min-height: 40px;
-`;
-
-const CardCat = styled.span.attrs((props) => ({
-  className: props.className || "",
-}))`
-  background-color: #775ebf;
-  padding: 0.1em 0.8em;
-  margin: 0 5px;
-  font-size: 0.8rem;
-  border-radius: var(--border-radius-small);
-
-  &:first-child {
-    margin-left: 0;
-  }
 `;
 
 const CardEdit = styled.span.attrs((props) => ({
@@ -42,7 +29,7 @@ export const CardTop = ({ cats, id }) => {
     <Top className="top">
       {" "}
       <div className="cat">
-        {cats && cats.map((cat, index) => <CardCat key={index}>{cat}</CardCat>)}
+        {cats && cats.map((cat, index) => <CatBtn key={index}>{cat}</CatBtn>)}
       </div>
       <CardEdit onClick={handleEditClick}>🖊️</CardEdit>
     </Top>
--- src/styles/Buttons.jsx
+++ src/styles/Buttons.jsx
@@ -11,3 +11,17 @@ export const SubmitBtn = styled.button.attrs((props) => ({
   margin-top: 1em;
   font-weight: 700;
 `;
+
+export const CatBtn = styled.span.attrs((props) => ({
+  className: props.className || "",
+}))`
+  background-color: var(--clr-cats);
+  padding: 0.1em 0.8em;
+  margin: 0 5px;
+  font-size: 0.8rem;
+  border-radius: var(--border-radius-small);
+
+  &:first-child {
+    margin-left: 0;
+  }
+`;
