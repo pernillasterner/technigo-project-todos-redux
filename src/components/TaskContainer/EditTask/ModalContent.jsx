d16ca992caec0f01cbe011d62bdc883b924df763
psterner <psterner@hotmail.com>
2023-11-24 12:06:40 +0100

    Update state with new content

--- src/components/TaskContainer/EditTask/ModalContent.jsx
+++ src/components/TaskContainer/EditTask/ModalContent.jsx
@@ -1,21 +1,50 @@
-export const ModalContent = ({ title }) => {
-  console.log(title);
+import styled from "styled-components";
+
+const TitleInput = styled.input.attrs((props) => ({
+  className: props.className || "",
+}))`
+  font-size: 1.4rem;
+`;
+
+export const ModalContent = ({
+  onTitleChange,
+  onContentChange,
+  title,
+  content,
+}) => {
+  const handleTitleInput = (e) => {
+    e.preventDefault();
+    const newTitle = e.target.value;
+    onTitleChange(newTitle);
+  };
+
+  const handleContentInput = (e) => {
+    e.preventDefault();
+    const newContent = e.target.value;
+    onContentChange(newContent);
+  };
+
   return (
     <>
       <label htmlFor="title" className="is-hidden">
         Title
       </label>
-      <input type="text" name="title" placeholder={title} />
+      <TitleInput
+        type="text"
+        name="title"
+        placeholder={title}
+        onChange={handleTitleInput}
+      />
 
-      <label htmlFor="textarea" className="is-hidden">
-        Textarea
-      </label>
+      <label htmlFor="content">Description</label>
       <textarea
-        name=""
+        type="text"
+        name="content"
         id=""
         cols="60"
         rows="7"
-        placeholder="Add content"
+        placeholder={content}
+        onChange={handleContentInput}
       ></textarea>
     </>
   );
