19dd2dd97775d68a2052c27c2630ce9a5b372080
psterner <psterner@hotmail.com>
2023-11-25 20:18:29 +0100

    Small styling fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 
 const HeaderContainer = styled.header`
   padding: var(--general-padding);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Header = () => {
