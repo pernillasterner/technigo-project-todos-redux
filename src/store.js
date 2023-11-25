4057cf640af654e675b18f96302e2bca9638c737
psterner <psterner@hotmail.com>
2023-11-25 20:13:24 +0100

    Remove filter slicer

--- src/store.js
+++ src/store.js
@@ -3,13 +3,11 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import taskReducer from "./reducers/task/taskSlice";
 import modalReducer from "./reducers/modal/modalSlice";
 // import { projects } from "./reducers/projects";
-// import { filters } from "./reducers/filters";
 
 const reducer = combineReducers({
   task: taskReducer,
   modal: modalReducer,
   // projects: projects.reducer,
-  // filters: filters.reducer,
 });
 
 export const store = configureStore({
