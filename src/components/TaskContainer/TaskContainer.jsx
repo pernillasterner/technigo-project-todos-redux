b78c9146b9fa84409afef2c0294722deff3e9f90
psterner <psterner@hotmail.com>
2023-12-03 17:34:44 +0100

    Add logic for selecting categories

--- src/components/TaskContainer/TaskContainer.jsx
+++ src/components/TaskContainer/TaskContainer.jsx
@@ -20,6 +20,7 @@ export const TaskContainer = () => {
   const taskId = useSelector((store) => store.modal.taskId);
   const isModalOpen = useSelector((store) => store.modal.isModalOpen);
   const option = useSelector((store) => store.filter.option);
+  const cat = useSelector((store) => store.filter.cat);
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
@@ -29,10 +30,17 @@ export const TaskContainer = () => {
     } else if (option === "uncompleted") {
       const uncompletedTasks = allTasks.filter((task) => !task.completed);
       setTasks(uncompletedTasks);
-    } else if (option === "all") {
+    } else if (cat !== "all") {
+      const catTasks = allTasks.filter((task) => task.category === cat);
+      if (catTasks.length > 0) {
+        setTasks(catTasks);
+      } else {
+        console.log("Could find task with chosen category");
+      }
+    } else if (option === "all" || cat === "all") {
       setTasks(allTasks);
     }
-  }, [option, allTasks]);
+  }, [option, allTasks, cat]);
 
   const taskColumns = [
     { title: "uncompleted", tasks: tasks.filter((task) => !task.completed) },
