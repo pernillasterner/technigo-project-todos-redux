e0508b5780caf8e422e51bed3689872869927c63
psterner <psterner@hotmail.com>
2023-12-03 17:35:39 +0100

    Add dropdown for filter categories

---
+++ src/components/Header/Category.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Select } from "../../styles/Select";
+import { filterCat } from "../../reducers/filter/filterSlice";
+
+export const Category = () => {
+  const dispatch = useDispatch();
+  const tasks = useSelector((store) => store.task.tasks);
+  const cats = useSelector((store) => store.filter.categories);
+
+  const handleCat = (e) => {
+    e.preventDefault();
+    const selectedCat = e.target.value;
+    dispatch(filterCat(selectedCat));
+  };
+  return (
+    <>
+      {tasks.length !== 0 && (
+        <Select name="filter" onChange={handleCat}>
+          <option value="all">all categories</option>
+          {cats.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </Select>
+      )}
+    </>
+  );
+};
