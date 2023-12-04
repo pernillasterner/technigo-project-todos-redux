c19c5cab213d78ca6821a29a2c23702c7155a77c
psterner <psterner@hotmail.com>
2023-12-04 22:52:27 +0100

    Set max-width to main

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import { Header } from "./components/Header/Header";
 import { TaskContainer } from "./components/TaskContainer/TaskContainer";
 
 const MAIN = styled.main`
-  padding: var(--general-padding);
+  padding: var(--general-padding) 45px;
   height: 100vh;
   display: flex;
   flex-direction: column;
+  max-width: 1728px;
 `;
 
 export const App = () => {
