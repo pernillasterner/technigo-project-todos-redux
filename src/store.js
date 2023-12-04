ef630533e0d66bc67920aa6e92961959419a6e7f
psterner <psterner@hotmail.com>
2023-12-04 22:51:04 +0100

    Add project reducer

--- src/store.js
+++ src/store.js
@@ -2,14 +2,14 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 import taskReducer from "./reducers/task/taskSlice";
 import modalReducer from "./reducers/modal/modalSlice";
-// import { projects } from "./reducers/projects";
+import projectReducer from "./reducers/project/projectSlice";
 import filterReducer from "./reducers/filter/filterSlice";
 
 const reducer = combineReducers({
   task: taskReducer,
   modal: modalReducer,
   filter: filterReducer,
-  // projects: projects.reducer,
+  project: projectReducer,
 });
 
 export const store = configureStore({
