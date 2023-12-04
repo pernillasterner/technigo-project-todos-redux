ef215d0c8b65a5426840d88e772356ddb9258fca
psterner <psterner@hotmail.com>
2023-12-04 23:03:32 +0100

    Add unique key to taskcard

--- src/components/TaskContainer/TaskColumn.jsx
+++ src/components/TaskContainer/TaskColumn.jsx
@@ -10,20 +10,27 @@ const StyledTaskColumn = styled.div.attrs((props) => ({
   display: flex;
   flex-direction: column;
   padding-left: var(--general-padding);
+
+  position: relative;
+  max-height: 1500px; /* Set a max height to enable scrolling */
+  overflow-y: auto;
 `;
 
 export const TaskColumn = ({ title, tasks, total }) => {
   return (
     <StyledTaskColumn className="task_column">
       <TopCard title={title} total={total} />
-      {title === "uncompleted" && (
+      {title !== "completed" && (
         <>
           <ClearTasks tasks={tasks} />
-          <AddTaskCard tasks={tasks} />
+          <AddTaskCard tasks={tasks} title={title} />
         </>
       )}
 
-      {tasks && tasks.map((task) => <TaskCard key={task.id} task={task} />)}
+      {tasks &&
+        tasks.map((task, index) => (
+          <TaskCard key={`card_${index}`} task={task} />
+        ))}
     </StyledTaskColumn>
   );
 };
