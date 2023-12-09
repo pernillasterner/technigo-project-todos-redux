e621da46b20bb7591aa65c795f96c2226692cdbe
psterner <psterner@hotmail.com>
2023-12-09 10:02:21 +0100

    Make header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const HeaderContainer = styled.header`
   padding: var(--general-padding);
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
 `;
